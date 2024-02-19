b7b3fc6708636180eaacdbcf0eeaa42f331e2846
SaranyaaUK <157544590+SaranyaaUK@users.noreply.github.com>
2024-02-19 21:12:42 +0000

    New Class creation and Player Class update
    Creating the new classes required to process the initialise event and update needed for the Player class.

---
+++ ITSD-DT2023-24-Template/ITSD-DT2023-24-Template/app/structures/basic/CardManager.java
@@ -0,0 +1,28 @@
+package structures.basic;
+// Import any packages as needed
+
+/**
+ * Help of the CardManager class goes here
+ *
+ */
+
+public class CardManager {
+
+    /*
+     * Signature
+     * 
+     * Attributes
+     * Collection<Card> myDeck
+     * Collection<Card> myHandCards
+     * final MAX_HAND_CARDS = 6
+     * 
+     * Methods
+     * 
+     * void generateDeck(boolean)
+     * void drawCardFromDeck(int)
+     * boolean isDeckEmpty()
+     * Collection<Card> getHandCards()
+     * 
+     */
+
+}
---
+++ ITSD-DT2023-24-Template/ITSD-DT2023-24-Template/app/structures/basic/GameLogic.java
@@ -0,0 +1,24 @@
+package structures.basic;
+// Import any packages as needed
+
+/**
+ * Help of the GameLogic class goes here
+ *
+ */
+
+public class GameLogic {
+
+    /*
+     * Signature
+     * 
+     * 
+     * Methods
+     * 
+     * // Take required information from the GameState
+     * 
+     * void drawGrid(actorRef)
+     * void drawCards(actorRef)
+     * 
+     */
+
+}
