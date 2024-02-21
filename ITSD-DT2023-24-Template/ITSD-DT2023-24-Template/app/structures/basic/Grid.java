cd4a0ec35bd44ae1a161cbd05ae9f17f4c6123e3
Weifan Ma <2921952m@student.gla.ac.uk>
2024-02-21 22:03:51 +0000

    I have added Grid Class and the methods for generate grid, get target tiles and get the grid.

--- ITSD-DT2023-24-Template/ITSD-DT2023-24-Template/app/structures/basic/Grid.java
+++ ITSD-DT2023-24-Template/ITSD-DT2023-24-Template/app/structures/basic/Grid.java
@@ -1,8 +1,12 @@
 package structures.basic;
 // Import any packages as needed
 
+import utils.BasicObjectBuilders;
+
 /**
- * Help of the Grid class goes here
+ * The grid class contains 2 division of tiles object (5*9).
+ * and contain the methods for generate the grid(board), get tile return the target tile,
+ * and method for return the grid
  *
  */
 
@@ -20,5 +24,28 @@ public class Grid {
      * Tile[][] getGrid()
      * 
      */
+    private Tile[][] tiles = null;
+    public Grid(){
+        tiles = new Tile[5][9];
+    }
+
+    public Tile getTiles(int x,int y) {
+        return tiles[x][y];
+    }
+    public Tile[][] getGrid(){
+        return tiles;
+    }
+    //The generateGrid method will generate back-end tiles
+    public void generateGrid(){
+       Tile tile;
+        for (int i = 0; i < tiles.length; i++) {
+            for(int j=0;j<tiles[i].length;j++){
+                tile = BasicObjectBuilders.loadTile(i,j);
+                tiles[i][j]=tile;
+            }
+        }
+    }
+
+
 
 }
